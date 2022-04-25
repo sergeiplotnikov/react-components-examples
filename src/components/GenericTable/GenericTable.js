4ab57039465c86f28154fb015e32a5bd16b2a2b0
sergeiplotnikov <sergei.plotnikov128@gmail.com>
2022-04-25 20:54:04 +0300

    1 file modified

--- src/components/GenericTable/GenericTable.js
+++ src/components/GenericTable/GenericTable.js
@@ -1,51 +1,54 @@
 import React from 'react';
 
 export default function GenericTable(props) {
-  if (props.groupBy) return <>{props.data.reduce(getUniqueValues(props.groupBy), []).map(renderGroup(props.data, props.header, props.groupBy))}</>;
-  return renderTable(props.data, props.header);
-};
-
-function renderGroup(tableData, tableHeader, groupBy) {
-  return function(groupingValue) {
-    return renderTable(tableData.filter(x => getPropertyFromObject(x, groupBy.split('.')) === groupingValue), tableHeader);
+  function renderGroup(groupingValue) {
+    return renderTable(
+      props.data.filter(
+        (x) =>
+          getPropertyFromObject(x, props.groupBy.split('.')) === groupingValue
+      ),
+      props.header
+    );
   }
+
+  if (props.groupBy)
+    return (
+      <>
+        {props.data.reduce(getUniqueValues(props.groupBy), []).map(renderGroup)}
+      </>
+    );
+  return renderTable(props.data, props.header);
+}
+
+function renderTable(tableData, tableHeader) {
+  function renderTableRow(dataRow) {
+    function renderCellByHeader(tableHeader) {
+      return (
+        <td>{getPropertyFromObject(dataRow, tableHeader.field.split('.'))}</td>
+      );
+    }
+    return <tr>{tableHeader.map(renderCellByHeader)}</tr>;
+  }
+  function renderColumnHeader(columnHeader) {
+    return <th>{columnHeader.title}</th>;
+  }
+
+  return (
+    <table border="1" style={{ width: '100%' }}>
+      <thead>{tableHeader.map(renderColumnHeader)}</thead>
+      <tbody>{tableData.map(renderTableRow)}</tbody>
+    </table>
+  );
 }
 
 function getUniqueValues(groupBy) {
   return function (acc, item) {
-    if (acc.includes(item[groupBy])) return acc;
-    return [...acc, item[groupBy]];
+    if (acc.includes(getPropertyFromObject(item, groupBy.split('.')))) return acc;
+    return [...acc, getPropertyFromObject(item, groupBy.split('.'))];
   };
-};
-
-function renderColumnHeader(columnHeader) {
-  return <th>{columnHeader.title}</th>;
-};
+}
 
 function getPropertyFromObject(obj, propertyPath) {
   if (propertyPath.length === 0) return obj;
   return getPropertyFromObject(obj[propertyPath[0]], propertyPath.slice(1));
-};
-
-function renderCellByHeader(dataRow) {
-  return function (tableHeader) {
-    return (
-      <td>{getPropertyFromObject(dataRow, tableHeader.field.split('.'))}</td>
-    );
-  };
-};
-
-function renderTableRow(tableHeader) {
-  return function (dataRow) {
-    return <tr>{tableHeader.map(renderCellByHeader(dataRow))}</tr>;
-  };
-};
-
-function renderTable(tableData, tableHeader) {
-  return (
-    <table border="1" style={{width: '100%'}}>
-      <thead>{tableHeader.map(renderColumnHeader)}</thead>
-      <tbody>{tableData.map(renderTableRow(tableHeader))}</tbody>
-    </table>
-  );
-};
+}
